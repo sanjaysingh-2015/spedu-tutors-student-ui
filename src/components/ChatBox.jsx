ac0b6f9ebe7e0f9b5051b6531dbc0ed4ef357e69
SanjayKumarSingh <sanjay.singh.1069@gmail.com>
2025-10-24 19:05:15 +0530

    Payment Gateway

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -3,7 +3,7 @@ import SockJS from "sockjs-client";
 import * as Stomp from "@stomp/stompjs";
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:8084/chat-api";
+const API_BASE = import.meta.env.VITE_CHAT_API_BASE_URL || "http://localhost:8084/chat-api";
 
 const ChatBox = ({ userId, tutorId, studentId }) => {
   const [stompClient, setStompClient] = useState(null);
