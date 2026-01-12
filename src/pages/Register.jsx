21fb167d974f839406a986c9b523dcce42f05baf
SanjayKumarSingh <sanjay.singh.1069@gmail.com>
2026-01-12 15:39:26 +0530

    Register page alignment

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -13,29 +13,33 @@ export default function Register() {
   };
 
   return (
-    <div className="max-w-md mx-auto mt-12 card">
-      <h2 className="text-xl mb-4">Register Yourself</h2>
-      <form onSubmit={handleSubmit} className="bg-white p-6 shadow rounded w-96">
-        <input type="text" placeholder="User Name"
-          value={form.name}
-          onChange={(e) => setForm({ ...form, name: e.target.value })}
-          className="border p-2 mb-2 w-full" />
-        <input type="email" placeholder="Email"
-          value={form.email}
-          onChange={(e) => setForm({ ...form, email: e.target.value })}
-          className="border p-2 mb-2 w-full" />
-        <input type="text" placeholder="Phone Number"
-          value={form.phoneNo}
-          onChange={(e) => setForm({ ...form, phoneNo: e.target.value })}
-          className="border p-2 mb-2 w-full" />
-        <input type="password" placeholder="Password"
-          value={form.password}
-          onChange={(e) => setForm({ ...form, password: e.target.value })}
-          className="border p-2 mb-4 w-full" />
-        <button type="submit" className="bg-blue-600 text-white px-4 py-2 rounded">
-          Register
-        </button>
-      </form>
+    <div className="flex justify-center px-4 pt-6">
+      <div className="w-full max-w-md bg-white shadow-lg rounded-lg p-6">
+        <h3 className="text-xl font-semibold mb-6 text-center">
+          Student Registration
+        </h3>
+        <form onSubmit={handleSubmit} className="bg-white p-6 shadow rounded w-96">
+          <input type="text" placeholder="User Name"
+            value={form.name}
+            onChange={(e) => setForm({ ...form, name: e.target.value })}
+            className="border p-2 mb-2 w-full" />
+          <input type="email" placeholder="Email"
+            value={form.email}
+            onChange={(e) => setForm({ ...form, email: e.target.value })}
+            className="border p-2 mb-2 w-full" />
+          <input type="text" placeholder="Phone Number"
+            value={form.phoneNo}
+            onChange={(e) => setForm({ ...form, phoneNo: e.target.value })}
+            className="border p-2 mb-2 w-full" />
+          <input type="password" placeholder="Password"
+            value={form.password}
+            onChange={(e) => setForm({ ...form, password: e.target.value })}
+            className="border p-2 mb-4 w-full" />
+          <button type="submit" className="bg-blue-600 text-white px-4 py-2 rounded">
+            Register
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
